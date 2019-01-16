23188b4ccbd315053f028869d89629fda17bbce5
hakim24 <mtaufiqalhakim24@gmail.com>
2019-01-16 22:28:18 +0700

    change title

--- backend/views/section/index.php
+++ backend/views/section/index.php
@@ -7,7 +7,7 @@ use yii\widgets\Pjax;
 /* @var $this yii\web\View */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = 'Seksi';
+$this->title = 'Department';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="section-index">
